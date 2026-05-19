02f1649097e017579c694f2205b3f2ea99e8d4a9
Manish Sharma <manishsharma.aice@gmail.com>
2026-05-19 16:06:30 +0530

    code update

--- src/Login.jsx
+++ src/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
 
             // Backend API call
             const response = await fetch(
-                "http://localhost:5000/api/auth/login",
+                "https://jwt-form-backend.onrender.com/login",
                 {
                     method: "POST",
 
--- src/Register.jsx
+++ src/Register.jsx
@@ -27,7 +27,7 @@ function Register() {
         try {
 
             const response = await fetch(
-                "http://localhost:5000/api/auth/register",
+                "https://jwt-form-backend.onrender.com",
                 {
                     method: "POST",
 
